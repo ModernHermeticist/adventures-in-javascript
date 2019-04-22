327f1b89c010e60657d80e6b3c7e26a347d8637a
Queen <QueenJ7@ncsd1.k12.wy.us>
2019-04-22 11:31:14 -0600

    Update a-star.js

--- a-star/a-star.js
+++ a-star/a-star.js
@@ -1,5 +1,23 @@
-var rows = 5;
-var cols = 5;
+function removeFromArray(arr, elt)
+{
+    for (var i = arr.length-1; i >= 0; i--)
+    {
+        if (arr[i] == elt)
+        {
+            arr.splice(i, 1);
+        }
+    }
+}
+
+function heuristic(a, b)
+{
+    //var d = dist(a.i, a.j, b.i, b.j);
+    var d = abs(a.i-b.i) + abs(a.j-b.j);
+    return d;
+}
+
+var rows = 15;
+var cols = 15;
 var grid = new Array(cols);
 
 var openSet = [];
@@ -9,19 +27,37 @@ var end;
 
 var w, h;
 
+var path = [];
+
 function Spot(i, j)
 {
-    this.x = i;
-    this.y = j;
+    this.i = i;
+    this.j = j;
     this.f = 0;
     this.g = 0;
     this.h = 0;
+    this.neighbors = [];
+    this.previous = undefined;
 
     this.show = function(col)
     {
         fill(col);
         noStroke();
-        rect(this.x * w, this.y * h, w-1, h-1);
+        rect(this.i * w, this.j * h, w-1, h-1);
+    }
+
+    this.addNeighbors = function(grid)
+    {
+        var i = this.i;
+        var j = this.j;
+        if (i < cols - 1)
+            this.neighbors.push(grid[i + 1][j]);
+        if (i > 0)
+            this.neighbors.push(grid[i - 1][j]);
+        if (j < rows - 1)
+            this.neighbors.push(grid[i][j + 1]);
+        if (j > 0)
+            this.neighbors.push(grid[i][j - 1]);
     }
 }
 
@@ -46,6 +82,14 @@ function setup()
         }
     }
 
+    for (var i = 0; i < cols; i++) 
+    {
+        for (var j = 0; j < rows; j++) 
+        {
+            grid[i][j].addNeighbors(grid);
+        }
+    }
+
     start = grid[0][0];
     end = grid[cols-1][rows-1];
 
@@ -67,11 +111,40 @@ function draw()
                 lowestIndex = i;
             }
         }
-    }
+        var current = openSet[lowestIndex];
 
-    if (openSet[lowestIndex] == end)
-    {
-        console.log("DONE");
+        if (current === end)
+        {
+            noLoop();
+            console.log("DONE");
+        }
+
+        removeFromArray(openSet, current);
+        closedSet.push(current);
+
+        var neighbors = current.neighbors;
+        for (var i = 0; i < neighbors.length; i++)
+        {
+            var neighbor = neighbors[i];
+
+            if(!closedSet.includes(neighbor))
+            {
+                var tG = current.g + 1;
+                if (openSet.includes(neighbor))
+                {
+                    if (tG < neighbor.g)
+                        neighbor.g = tG;
+                }
+                else
+                {
+                    neighbor.g = tG;
+                    openSet.push(neighbor);
+                }
+                neighbor.h = heuristic(neighbor,end);
+                neighbor.f = neighbor.g + neighbor.h;
+                neighbor.previous = current;
+            }
+        }
     }
     else
     {
@@ -95,5 +168,19 @@ function draw()
     for (var i = 0; i < openSet.length; i++)
     {
         openSet[i].show(color(0, 255, 0));
+    }
+
+    // Find best path
+    path = [];
+    var t = current;
+    path.push(t);
+    while (t.previous) {
+        path.push(t.previous);
+        t = t.previous;
+    }
+
+    for (var i = 0; i < path.length; i++)
+    {
+        path[i].show(color(0, 0, 255));
     }
 }
